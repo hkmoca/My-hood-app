aee6ce97076e964499a779608570e507e1d8711f
Héctor Moreno <hmoreno@nearsoft.com>
2016-10-10 15:08:51 -0700

    Create Post Class

--- My-Hood/Post.swift
+++ My-Hood/Post.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+class Post {
+    private var imagePath: String
+    private var title: String
+    private var postDesc: String
+    
+    
+    init(imagePath: String, title: String, description: String){
+        self.imagePath = imagePath
+        self.title = title
+        self.postDesc = description 
+    
+    }
+
+}

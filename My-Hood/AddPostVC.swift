cbf17bd60066c66ce9b233e31b8b663ea7200d10
Héctor Moreno <hmoreno@nearsoft.com>
2016-10-11 16:46:12 -0700

    Create AddPost Class

--- My-Hood/AddPostVC.swift
+++ My-Hood/AddPostVC.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class AddPostVC: UIViewController {
 
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        postImg.layer.cornerRadius = postImg.frame.width/2
+        postImg.clipsToBounds = true
+       
     }
 
+    @IBAction func makePostBtnPressed(sender: AnyObject) {
+        
+    }
    
+    @IBAction func cancelBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func addPicBtnPressed(sender: UIButton) {
+        sender.setTitle("", forState: .Normal)
+    }
 }

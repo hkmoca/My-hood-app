61f98d2366739ff6720efc81c7971cb668128de0
Héctor Moreno <hmoreno@nearsoft.com>
2016-10-10 18:14:34 -0700

    Add tableView extensions, Create objets to test

--- My-Hood/ViewController.swift
+++ My-Hood/ViewController.swift
@@ -8,20 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+     var posts = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let post = Post(imagePath: "", title: "Post 1", description: "Post 1 description")
+        let post2 = Post(imagePath: "", title: "Post 2", description: "Post 2 description")
+        let post3 = Post(imagePath: "", title: "Post 3", description: "Post 3 description")
+        
+        posts.append(post)
+        posts.append(post2)
+        posts.append(post3)
+        
+        tableView.reloadData()
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 90.0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            cell.configureCell(post)
+            return cell
+        } else  {
+            var cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+    }
+    
+    
 }
 

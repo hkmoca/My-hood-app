20e8214bd9fff169d876371b1432904162bfea14
Héctor Moreno <hmoreno@nearsoft.com>
2016-10-12 11:42:15 -0700

    Save Object with NSUserDefaults

--- My-Hood/DataService.swift
+++ My-Hood/DataService.swift
@@ -12,6 +12,7 @@ import UIKit
 class DataService {
 
     static let instance = DataService()
+    let KEY_POSTS = "posts"
     
     private var _loadedPosts = [Post]()
     
@@ -20,18 +21,47 @@ class DataService {
     }
     
     func savePosts() {
-    
+        let postsData = NSKeyedArchiver.archivedDataWithRootObject(_loadedPosts)
+        NSUserDefaults.standardUserDefaults().setObject(postsData, forKey: KEY_POSTS)
+        NSUserDefaults.standardUserDefaults().synchronize()
     }
     
     func loadPosts() {
-    
+        if let  postsData = NSUserDefaults.standardUserDefaults().objectForKey(KEY_POSTS) as? NSData {
+            if let postsArray = NSKeyedUnarchiver.unarchiveObjectWithData(postsData) as? [Post]{
+                _loadedPosts = postsArray
+            }
+        }
+        
+        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "postsLoaded", object: nil))
+        
     }
     
-    func saveImageAndCreatePath(image: UIImage){
-    
+    func saveImageAndCreatePath(image: UIImage) -> String {
+        let imageData = UIImagePNGRepresentation(image)
+        let imgePath = "image\(NSDate.timeIntervalSinceReferenceDate()).png"
+        let fullPath = documentsPathForFileName(imgePath)
+        imageData?.writeToFile(fullPath, atomically: true)
+        return imgePath
     }
     
-    func imageForPath(path: String){
+    func imageForPath(path: String) -> UIImage? {
+        let fullPath = documentsPathForFileName(path)
+        let image = UIImage(named: fullPath)
+        return image
+    }
     
+    func  addPost(post: Post){
+        _loadedPosts.append(post)
+        savePosts()
+        loadPosts()
+    }
+    
+    
+    
+    func documentsPathForFileName(name: String) -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let fullPath = paths[0] as NSString
+        return fullPath.stringByAppendingPathComponent(name)
     }
 }
